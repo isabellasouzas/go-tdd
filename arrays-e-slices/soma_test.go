a7ba15ae7cf2bf23ac556045a44e863b9e97932b
isabellasouzas <isabsantos92@gmail.com>
2020-11-16 12:41:14 -0300

    feat: create soma function and test

---
+++ arrays-e-slices/soma_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	t.Run("coleção de 5 numeros", func(t *testing.T) {
+		numeros := []int{1, 2, 3, 4, 5}
+
+		resultado := Soma(numeros)
+		esperado := 15
+
+		if esperado != resultado {
+			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
+		}
+	})
+	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
+		numeros := []int{1, 2, 3}
+
+		resultado := Soma(numeros)
+		esperado := 6
+
+		if resultado != esperado {
+			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
+		}
+	})
+}
