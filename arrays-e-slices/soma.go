7b1a27c161b19d90454cdb44880604daad8058f2
isabellasouzas <isabsantos92@gmail.com>
2020-11-16 18:23:30 -0300

    feat: create SomaTodoOResto function and test

--- arrays-e-slices/soma.go
+++ arrays-e-slices/soma.go
@@ -7,3 +7,28 @@ func Soma(numeros []int) int {
 	}
 	return soma
 }
+
+//func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+//	quantidadeDeNumeros := len(numerosParaSomar)
+//	somas = make([]int, quantidadeDeNumeros)
+//
+//	for _, numeros := range numerosParaSomar {
+//		somas = append(somas, Soma(numeros))
+//	}
+//
+//	return somas
+//}
+
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+
+	return somas
+}
