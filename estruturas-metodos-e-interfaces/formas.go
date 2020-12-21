8f6eba5e57c078caf092458da97e269a41a079fa
Isabella Souza <isabsantos92@gmail.com>
2020-12-21 19:56:45 -0300

    feat: cria interface forma

--- estruturas-metodos-e-interfaces/formas.go
+++ estruturas-metodos-e-interfaces/formas.go
@@ -1,14 +1,20 @@
 package estruturas_metodos_e_interfaces
 
+import "math"
+
+type Forma interface {
+	Area() float64
+}
+
 type Retangulo struct {
 	Largura float64
 	Altura  float64
 }
 
-func Perimetro(retangulo Retangulo) float64 {
-	return 2 * (retangulo.Largura + retangulo.Altura)
+type Circulo struct {
+	Raio float64
 }
 
-func Area(retangulo Retangulo) float64 {
-	return retangulo.Largura * retangulo.Altura
+func (r Retangulo) Area() float64 {
+	return math.Pi * c.Raio * c.Raio
 }
