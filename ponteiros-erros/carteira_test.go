ec6901dce91e8aa0146cb921710bd865ab19c885
Isabella Souza <isabsantos92@gmail.com>
2020-12-22 19:10:57 -0300

    test: adiciona testes a carteira

---
+++ ponteiros-erros/carteira_test.go
@@ -0,0 +1,21 @@
+package ponteiros_erros
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCarteira(t *testing.T) {
+	carteira := Carteira{}
+
+	carteira.Depositar(10)
+
+	resultado := carteira.Saldo()
+
+	fmt.Printf("O endereço do saldo no teste é %v \n", &carteira.saldo)
+	esperado := 10
+
+	if resultado != esperado {
+		t.Errorf("resultado %d, esperado %d", resultado, esperado)
+	}
+}
