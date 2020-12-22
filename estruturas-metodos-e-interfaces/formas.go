39973bbbe7c846dfdb22bda9641b76ce28d21fa1
Isabella Souza <isabsantos92@gmail.com>
2020-12-21 21:01:58 -0300

    feat: adiciona forma triangulo

--- estruturas-metodos-e-interfaces/formas.go
+++ estruturas-metodos-e-interfaces/formas.go
@@ -15,10 +15,19 @@ type Circulo struct {
 	Raio float64
 }
 
+type Triangulo struct {
+	Base   float64
+	Altura float64
+}
+
 func (r Retangulo) Area() float64 {
 	return r.Largura * r.Altura
 }
 
 func (c Circulo) Area() float64 {
 	return math.Pi * c.Raio * c.Raio
+}
+
+func (t Triangulo) Area() float64 {
+	return (t.Base * t.Altura) * 0.5
 }
