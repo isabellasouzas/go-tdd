cf3feff22967d8ce7da22661928dabd9ff02b44c
Isabella Souza <isabsantos92@gmail.com>
2020-12-22 19:17:27 -0300

    feat: Adiciona type bitcoin

--- ponteiros-erros/carteira.go
+++ ponteiros-erros/carteira.go
@@ -2,15 +2,25 @@ package ponteiros_erros
 
 import "fmt"
 
-type Carteira struct {
-	saldo int
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (c *Carteira) Depositar(quantidade int) {
+type Carteira struct {
+	saldo Bitcoin
+}
+
+func (c *Carteira) Depositar(quantidade Bitcoin) {
 	fmt.Printf("O endereço do saldo no Depositar é %v \n", &c.saldo)
 	c.saldo += quantidade
 }
 
 func (c *Carteira) Saldo() int {
 	return c.saldo
+}
+
+func (b Bitcoin) String() string {
+	fmt.Sprintf("%d BTC", b)
 }
