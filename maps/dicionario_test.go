539cf038ecca677079bae53af14c0c9286f65942
Isabella Souza <isabsantos92@gmail.com>
2020-12-25 20:26:20 -0300

    refactor: refatora teste Busca e cria funçao comparaString

--- maps/dicionario_test.go
+++ maps/dicionario_test.go
@@ -3,12 +3,18 @@ package maps
 import "testing"
 
 func TestBusca(t *testing.T) {
-	dicionario := map[string]string{"teste": "isso é apenas um teste"}
+	dicionario := Dicionario{"teste": "isso é apenas um teste"}
 
-	resultado := Busca(dicionario, "teste")
+	resultado := Dicionario.Busca(dicionario, "teste")
 	esperado := "isso é apenas um teste"
 
+	comparaStrings(t, resultado, esperado)
+}
+
+func comparaStrings(t *testing.T, resultado, esperado string) {
+	t.Helper()
+
 	if resultado != esperado {
-		t.Errorf("resultado '%s',esperado %s, dado '%s'", resultado, esperado, "test")
+		t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "teste")
 	}
 }
