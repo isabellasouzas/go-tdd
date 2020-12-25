928bd3c7be4ee257c9bf84287150bb909f08b1d1
Isabella Souza <isabsantos92@gmail.com>
2020-12-25 20:56:54 -0300

    feat: cria funcao adiciona

--- maps/dicionario.go
+++ maps/dicionario.go
@@ -1,7 +1,20 @@
 package maps
 
+import "errors"
+
 type Dicionario map[string]string
 
-func (d Dicionario) Busca(palavra string) string {
-	return d[palavra]
+var ErrNaoEncontrado = errors.New("não foi possível encontrar a palavra que você procura")
+
+func (d Dicionario) Busca(palavra string) (string, error) {
+	definicao, existe := d[palavra]
+	if !existe {
+		return "", ErrNaoEncontrado
+	}
+
+	return definicao, nil
+}
+
+func (d Dicionario) Adiciona(palavra, definicao string) {
+	d[palavra] = definicao
 }
