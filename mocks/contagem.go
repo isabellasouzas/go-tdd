543c1b4205f736b5d0c7f9c44ad28bf51b10dc59
Isabella Souza <isabsantos92@gmail.com>
2020-12-27 14:06:26 -0300

    feat: implementa spyContagemOperacoes

--- mocks/contagem.go
+++ mocks/contagem.go
@@ -16,11 +16,26 @@ type SleeperSpy struct {
 	Chamadas int
 }
 
+type SpyContagemOperacoes struct {
+	Chamadas []string
+}
+
 type SleeperPadrao struct {
 }
 
 const ultimaPalavra = "Go!"
 const inicioContagem = 3
+const escrita = "escrita"
+const pausa = "pausa"
+
+func (s *SpyContagemOperacoes) Pausa() {
+	s.Chamadas = append(s.Chamadas, pausa)
+}
+
+func (s *SpyContagemOperacoes) Write(p []byte) (n int, err error) {
+	s.Chamadas = append(s.Chamadas, escrita)
+	return
+}
 
 func (s *SleeperSpy) Sleep() {
 	s.Chamadas++
@@ -32,10 +47,15 @@ func (d *SleeperPadrao) Sleep() {
 
 func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
-		sleeper.Sleep()
+		sleeper.Pausa()
 		fmt.Fprintln(saida, i)
 	}
-	sleeper.Sleep()
+
+	for i := inicioContagem; i > 0; i-- {
+		fmt.Fprintln(saida, i)
+	}
+
+	sleeper.Pausa()
 	fmt.Fprint(saida, ultimaPalavra)
 }
 
