df303ba176c6e595c5e9f66baad67fbbad4bb96f
Isabella Souza <isabsantos92@gmail.com>
2020-12-27 20:59:58 -0300

    refactor: refatora funcao VerificaWebsites()

--- concorrencia/verificaWebsites.go
+++ concorrencia/verificaWebsites.go
@@ -1,12 +1,19 @@
 package concorrencia
 
+import "time"
+
 type VerificadorWebsites func(string) bool
 
 func VerificaWebsites(vw VerificadorWebsites, urls []string) map[string]bool {
 	resultados := make(map[string]bool)
 
 	for _, url := range urls {
-		resultados[url] = vw(url)
+		go func(u string) {
+			resultados[u] = vw(u)
+		}(url)
 	}
+
+	time.Sleep(2 * time.Second)
+
 	return resultados
 }
