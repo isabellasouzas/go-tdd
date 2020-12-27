c377969ec05dc5287a571cb14299646d74e8425c
Isabella Souza <isabsantos92@gmail.com>
2020-12-27 15:21:19 -0300

    test: testa verificaWebsites usando mock

---
+++ concorrencia/verificaWebsites_test.go
@@ -0,0 +1,32 @@
+package concorrencia
+
+import (
+	"reflect"
+	"testing"
+)
+
+func mockVerificadorWebsite(url string) bool {
+	if url == "waat:fufhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestVerificaWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	esperado := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+	resultado := VerificaWebsites(mockVerificadorWebsite, websites)
+
+	if !reflect.DeepEqual(esperado, resultado) {
+		t.Errorf("esperado %v, resultado %v", esperado, resultado)
+	}
+}
