f14048674aa1d5cdf8e198ba52495a1fba69841f
Isabella Souza <isabsantos92@gmail.com>
2020-12-28 17:46:27 -0300

    refactor: refatora funcao corredor

--- select/corredor.go
+++ select/corredor.go
@@ -6,14 +6,20 @@ import (
 	"time"
 )
 
+var limiteDeDezSegundos = 10 * time.Second
+
 func Corredor(a, b string) (vencedor string, erro error) {
+	return Configuravel(a, b, limiteDeDezSegundos)
+}
+
+func Configuravel(a, b string, tempoLimite time.Duration) (vencedor string, erro error) {
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
-		return "", fmt.Errorf("tempo limite de espera excedido para %s e %$", a, b)
+	case <-time.After(tempoLimite):
+		return "", fmt.Errorf("tempo limite de espera excedido para %s e %s", a, b)
 	}
 }
 
--- select/corredor_test.go
+++ select/corredor_test.go
@@ -8,24 +8,39 @@ import (
 )
 
 func TestCorredor(t *testing.T) {
+	t.Run("retorna um erro se o servidor não responder dentro de 10s", func(t *testing.T) {
+		servidorLento := criarServidorComAtraso(20 * time.Microsecond)
+		servidorRapido := criarServidorComAtraso(0 * time.Microsecond)
 
-	servidorLento := criarServidorComAtraso(20 * time.Microsecond)
-	servidorRapido := criarServidorComAtraso(0 * time.Microsecond)
+		defer servidorLento.Close()
+		defer servidorRapido.Close()
 
-	defer servidorLento.Close()
-	defer servidorRapido.Close()
+		URLLenta := servidorLento.URL
+		URLRapida := servidorRapido.URL
 
-	URLLenta := servidorLento.URL
-	URLRapida := servidorRapido.URL
+		esperado := URLRapida
+		resultado, err := Corredor(URLLenta, URLRapida)
 
-	esperado := URLRapida
-	resultado := Corredor(URLLenta, URLRapida)
+		if err != nil {
+			t.Fatalf("esperava um erro, mas não obtive um %v", err)
+		}
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-	}
-	servidorLento.Close()
-	servidorRapido.Close()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
+	t.Run("retorna um erro se o servidor não responder dentro de 10s", func(t *testing.T) {
+		servidor := criarServidorComAtraso(25 * time.Millisecond)
+
+		defer servidor.Close()
+
+		_, err := Configuravel(servidor.URL, servidor.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("esperava um erro, mas não obtive um")
+		}
+
+	})
 }
 
 func criarServidorComAtraso(atraso time.Duration) *httptest.Server {
