45cddde377c215be1574c891caa8489593f96174
Isabella Souza <isabsantos92@gmail.com>
2020-12-27 21:59:40 -0300

    refactor: refatora teste

--- select/corredor_test.go
+++ select/corredor_test.go
@@ -1,15 +1,33 @@
 package _select
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestCorredor(t *testing.T) {
-	URLLenta := "http://www.facebook.com"
-	URLRapida := "http://www.quii.co.uk"
+
+	servidorLento :=httptest.NewServer(http.HandlerFunc(w http.ResponseWriter, r *http.Request){
+		time.Sleep( 20 * time.Microsecond)
+		w.WriteHeader(http.StatusOK)
+	})
+
+	servidorRapido := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	URLLenta := servidorLento
+	URLRapida := servidorRapido
 
 	esperado := URLRapida
 	resultado := Corredor(URLLenta, URLRapida)
 
+
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 	}
+	servidorLento.Close()
+	servidorRapido.Close()
 }
