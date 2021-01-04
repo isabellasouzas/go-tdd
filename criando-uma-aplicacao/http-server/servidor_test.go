a89527603d30f741f88238b2561f09c62565277f
Isabella Souza <isabsantos92@gmail.com>
2021-01-04 14:31:00 -0300

    refactor: refatora teste

--- criando-uma-aplicacao/http-server/servidor_test.go
+++ criando-uma-aplicacao/http-server/servidor_test.go
@@ -8,21 +8,45 @@ import (
 )
 
 func TestObterJogador(t *testing.T) {
+	armazenamento := EsbocoArmazenamentoJogador{
+		map[string]int{
+			"Maria": 20,
+			"Pedro": 10,
+		},
+	}
+
+	servidor := &ServidorJogador{&armazenamento}
+
 	t.Run("retornar resultado de Maria", func(t *testing.T) {
 		requisicao := novaRequisicaoObterPontuacao("Maria")
 		resposta := httptest.NewRecorder()
 
-		ServidorJogador(resposta, requisicao)
+		servidor.ServerHTTP(resposta, requisicao)
 
+		verificarRespostaCodigoStatus(t, resposta.Code, http.StatusOK)
 		verificarCorpoRequisicao(t, resposta.Body.String(), "20")
 	})
 	t.Run("retornar resultado Pedro", func(t *testing.T) {
 		requisicao := novaRequisicaoObterPontuacao("Pedro")
 		resposta := httptest.NewRecorder()
 
-		ServidorJogador(resposta, requisicao)
+		server.ServerHTTP(resposta, requisicao)
 
+		verificarRespostaCodigoStatus(t, resposta.Code, http.StatusOK)
 		verificarCorpoRequisicao(t, resposta.Body.String(), "10")
+	})
+	t.Run("retorna 404 para jogador não encontrado", func(t *testing.T) {
+		requisicao := novaRequisicaoObterPontuacao("jorge")
+		resposta := httptest.NewRecorder()
+
+		server.ServerHTTP(resposta, requisicao)
+
+		recebido := resposta.Code
+		esperado := http.StatusNotFound
+
+		if recebido != esperado {
+			t.Errorf("recebido status '%d', esperado '%d'", recebido, esperado)
+		}
 	})
 }
 
@@ -35,5 +59,21 @@ func verificarCorpoRequisicao(t *testing.T, recebido, esperado string) {
 	t.Helper()
 	if recebido != esperado {
 		t.Errorf("corpo da requisicao é invalido, obtive '%s' esperava %s", recebido, esperado)
+	}
+}
+
+type EsbocoArmazenamentoJogador struct {
+	pontuacoes map[string]int
+}
+
+func (e *EsbocoArmazenamentoJogador) ObterPontuacaoJogador(nome string) int {
+	pontuacao := e.pontuacoes[nome]
+	return pontuacao
+}
+
+func verificarRespostaCodigoStatus(t *testing.T, recebido, esperado int) {
+	t.Helper()
+	if recebido != esperado {
+		t.Errorf("não recebeu o codigo de status http esperado, recebido %d, esperado %d", recebido, esperado)
 	}
 }
