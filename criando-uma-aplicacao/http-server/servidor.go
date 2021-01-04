72ddea4c53121a609f88da6b56528520216a1245
Isabella Souza <isabsantos92@gmail.com>
2021-01-04 14:47:56 -0300

    feat: adiciona metodo registrVitoria e mostraPontuacao

--- criando-uma-aplicacao/http-server/servidor.go
+++ criando-uma-aplicacao/http-server/servidor.go
@@ -23,3 +23,23 @@ func (s *ServidorJogador) ServeHTTP(writer http.ResponseWriter, request *http.Re
 	}
 	fmt.Fprint(writer, pontuacao)
 }
+
+func (s *ServidorJogador) registrarVitoria(writer http.ResponseWriter, request *http.Request) {
+	jogador := request.URL.Path[len("/jogadores/"):]
+
+	switch request.Method {
+	case http.MethodPost:
+		s.registrarVitoria(writer, jogador)
+	case http.MethodGet:
+		s.mostrarPontuacao(writer, jogador)
+	}
+}
+
+func (s *ServidorJogador) mostrarPontuacao(writer http.ResponseWriter, jogador string) {
+	pontuacao := s.armazenamento.ObterPontuacaoJogador(jogador)
+
+	if pontuacao == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(writer, jogador)
+}
