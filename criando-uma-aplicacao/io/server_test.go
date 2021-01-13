c7702ccd3950816306ea5b3478bf83effbae0134
Isabella Souza <isabsantos92@gmail.com>
2021-01-13 15:20:52 -0300

    refactor: refact server test

--- criando-uma-aplicacao/io/server_test.go
+++ criando-uma-aplicacao/io/server_test.go
@@ -1,68 +1,178 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"io/ioutil"
-	"os"
-	"strings"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
-func TestSistemaDeArquivoDermazenamento(t *testing.T) {
-	t.Run("/liga de um leitor", func(t *testing.T) {
-		bancoDeDados := strings.NewReader(`[
-				{"Nome": "Cleo", "Vitorias": 10},
-				{"Nome": "Chris", "Vitorias": 33}]`)
-
-		armazenamento := SistemasDeArquivoDeArmazenamentoDoJogador{bancoDeDados}
-
-		recebido := armazenamento.PegaLiga()
-
-		esperado := []Jogador{
-			{"Cleo", 10},
-			{"Chris", 33},
-		}
-
-		defineLiga(t, recebido, esperado)
-
-		//read again
-		recebido = armazenamento.PegaLiga()
-		defineLiga(t, recebido, esperado)
-	})
-	t.Run("pegar pontuação do jogador", func(t *testing.T ){
-		bancoDeDados := strings.NewReader( `[
-					{"Nome": "Cleo", "Vitorias": 10},
-					{"Nome": "Chris", "Vitorias": 33}]`)
-		})
-	armazenamento := SistemaDeArquivoDeArmazenamentoDoJogador{bancoDeDados}
-
-	recebido := armazenamento.("Chris")
-	esperado := 33
-
-	definePontuacaoIgual(t, recebido, esperado)
-
+type EsbocoDoArmazenamentoDoJogador struct {
+	pontuacoes        map[string]int
+	chamadasDeVitoria []string
+	liga              []Jogador
 }
 
-func (f *SistemaDeArquivoDeArmazenamentoDoJogador) PegaLiga() []Jogador {
-	liga, _ := NovaLiga(f.bancoDeDados)
+func (s *EsbocoDoArmazenamentoDoJogador) PegaPontuacaoDoJogador(nome string) int {
+	pontuacao := s.pontuacoes[nome]
+	return pontuacao
+}
+
+func (s *EsbocoDoArmazenamentoDoJogador) SalvaVitoria(nome string) {
+	s.chamadasDeVitoria = append(s.chamadasDeVitoria, nome)
+}
+
+func (s *EsbocoDoArmazenamentoDoJogador) PegaLiga() Liga {
+	return s.liga
+}
+
+func TestPegaJogadores(t *testing.T) {
+	armazenamento := EsbocoDoArmazenamentoDoJogador{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+		nil,
+		nil,
+	}
+	servidor := NovoServidorDoJogador(&armazenamento)
+
+	t.Run("retorna a pontuacao de Pepper'", func(t *testing.T) {
+		requisicao := novaRequisicaoPegaPontuacao("Pepper")
+		resposta := httptest.NewRecorder()
+
+		servidor.ServeHTTP(resposta, requisicao)
+
+		defineStatus(t, resposta.Code, http.StatusOK)
+		definecorpodeResposta(t, resposta.Body.String(), "20")
+	})
+
+	t.Run("retorna a pontuacao de Floyd", func(t *testing.T) {
+		requisicao := novaRequisicaoPegaPontuacao("Floyd")
+		resposta := httptest.NewRecorder()
+
+		servidor.ServeHTTP(resposta, requisicao)
+
+		defineStatus(t, resposta.Code, http.StatusOK)
+		definecorpodeResposta(t, resposta.Body.String(), "10")
+	})
+
+	t.Run("retorna 404 para jogadores nao existentes", func(t *testing.T) {
+		requisicao := novaRequisicaoPegaPontuacao("Apollo")
+		resposta := httptest.NewRecorder()
+
+		servidor.ServeHTTP(resposta, requisicao)
+
+		defineStatus(t, resposta.Code, http.StatusNotFound)
+	})
+}
+
+func TestArmazenamentoDeVitorias(t *testing.T) {
+	armazenamento := EsbocoDoArmazenamentoDoJogador{
+		map[string]int{},
+		nil,
+		nil,
+	}
+	servidor := NovoServidorDoJogador(&armazenamento)
+
+	t.Run("salva vitorias no POST", func(t *testing.T) {
+		jogador := "Pepper"
+
+		requisicao := novaRequisicaoPostaVitoria(jogador)
+		resposta := httptest.NewRecorder()
+
+		servidor.ServeHTTP(resposta, requisicao)
+
+		defineStatus(t, resposta.Code, http.StatusAccepted)
+
+		if len(armazenamento.chamadasDeVitoria) != 1 {
+			t.Fatalf("recebido %d chamadas para salvaVitoria esperado %d", len(armazenamento.chamadasDeVitoria), 1)
+		}
+
+		if armazenamento.chamadasDeVitoria[0] != jogador {
+			t.Errorf("nao armazenou vencedor correto recebido '%s' esperado '%s'", armazenamento.chamadasDeVitoria[0], jogador)
+		}
+	})
+}
+
+func TestLiga(t *testing.T) {
+
+	t.Run("retorna tabela de liga como JSON", func(t *testing.T) {
+		ligaDesejada := []Jogador{
+			{"Cleo", 32},
+			{"Chris", 20},
+			{"Tiest", 14},
+		}
+
+		armazenamento := EsbocoDoArmazenamentoDoJogador{nil, nil, ligaDesejada}
+		server := NovoServidorDoJogador(&armazenamento)
+
+		requisicao := requisicaoNovaLiga()
+		resposta := httptest.NewRecorder()
+
+		server.ServeHTTP(resposta, requisicao)
+
+		recebido := pegaLigaDaResposta(t, resposta.Body)
+
+		defineStatus(t, resposta.Code, http.StatusOK)
+		defineLiga(t, recebido, ligaDesejada)
+		defineTipoDeConteudo(t, resposta, jsonContentType)
+
+	})
+}
+
+func defineTipoDeConteudo(t *testing.T, resposta *httptest.ResponseRecorder, esperado string) {
+	t.Helper()
+	if resposta.Header().Get("content-type") != esperado {
+		t.Errorf("resposta nao tinha content-type de %s, recebido %v", esperado, resposta.HeaderMap)
+	}
+}
+
+func pegaLigaDaResposta(t *testing.T, body io.Reader) []Jogador {
+	t.Helper()
+	liga, err := NovaLiga(body)
+
+	if err != nil {
+		t.Fatalf("Nao foi possivel passar resposta do servidor '%s' em pedaco de jogador, '%v'", body, err)
+	}
+
 	return liga
 }
 
-func criaArquivoTemporario(t *testing.T, dadoInicial string) (io.ReadWriteSeeker,  func())  {
+func defineLiga(t *testing.T, recebido, esperado []Jogador) {
 	t.Helper()
-
-	arquivotmp, err := ioutil.TempFile("", "db")
-
-	if err != nil {
-		t.Fatalf("não foi possivel escrever o arquivo temporário %v", err)
+	if !reflect.DeepEqual(recebido, esperado) {
+		t.Errorf("recebido %v esperado %v", recebido, esperado)
 	}
+}
 
-	arquivotmp.Write([]byte(dadoInicial))
-
-	removeArquivo := func() {
-		arquivotmp.Close()
-		os.Remove(arquivotmp.Name())
+func defineStatus(t *testing.T, recebido, esperado int) {
+	t.Helper()
+	if recebido != esperado {
+		t.Errorf("nao recebeu status correto, recebido %d, esperado %d", recebido, esperado)
 	}
+}
 
-	return arquivotmp, removeArquivo
+func requisicaoNovaLiga() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/liga", nil)
+	return req
+}
+
+func novaRequisicaoPegaPontuacao(nome string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/jogadores/%s", nome), nil)
+	return req
+}
+
+func novaRequisicaoPostaVitoria(nome string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/jogadores/%s", nome), nil)
+	return req
+}
+
+func definecorpodeResposta(t *testing.T, recebido, esperado string) {
+	t.Helper()
+	if recebido != esperado {
+		t.Errorf("corpo da resposta esta errado, recebido '%s' esperado '%s'", recebido, esperado)
+	}
 }
