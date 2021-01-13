f6c4ee8a545cebe55ec55984571d5891b941402e
Isabella Souza <isabsantos92@gmail.com>
2021-01-13 15:12:10 -0300

    refactor: update main file

--- criando-uma-aplicacao/io/main.go
+++ criando-uma-aplicacao/io/main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	servidor := NovoServidorDoJogador(NovoArmazenamentoDeJogadorNaMemoria())
+	const dbFileName = "game.db.json"
 
-	if err := http.ListenAndServe(":5000", servidor); err != nil {
-		log.Fatalf("Não foi possivel ouvir na porta 5000 %v", err)
-	}
+	func main() {
+		db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+		if err != nil {
+			log.Fatalf("problema abrindo %s %v", dbFileName, err)
+		}
+
+		armazenamento, err := NovoSistemaDeArquivoDeArmazenamentoDoJogador(db)
+
+		if err != nil {
+			log.Fatalf("problema criando sistema de arquivo de armazenamento, %v ", err)
+		}
+
+		servidor := NovoServidorDoJogador(armazenamento)
+
+		if err := http.ListenAndServe(":5000", servidor); err != nil {
+			log.Fatalf("Não foi possivel ouvir na porta 5000 %v", err)
+		}
 }
