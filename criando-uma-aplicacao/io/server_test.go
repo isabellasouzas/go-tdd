586f7170bcf19600fd6609f6c6e35bbb22d5d60d
Isabella Souza <isabsantos92@gmail.com>
2021-01-13 12:25:42 -0300

    feat: replica codigos anteriores

---
+++ criando-uma-aplicacao/io/server_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSistemaDeArquivoDermazenamento(t *testing.T) {
+	t.Run("/liga de um leitor", func(t *testing.T) {
+		bancoDeDados := strings.NewReader(`[
+				{"Nome": "Cleo", "Vitorias": 10},
+				{"Nome": "Chris", "Vitorias": 33}]`)
+
+		armazenamento := SistemasDeArquivoDeArmazenamentoDoJogador{bancoDeDados}
+
+		recebido := armazenamento.PegaLiga()
+
+		esperado := []Jogador{
+			{"Cleo", 10},
+			{"Chris", 33},
+		}
+
+		defineLiga(t, recebido, esperado)
+	})
+}
