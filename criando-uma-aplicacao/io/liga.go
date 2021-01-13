6336562100efb0ca3b1b3fdd37154441257caebe
Isabella Souza <isabsantos92@gmail.com>
2021-01-13 12:25:21 -0300

    feat: adiciona NovaLiga()

---
+++ criando-uma-aplicacao/io/liga.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+func NovaLiga(rdr io.Reader) ([]Jogador, error) {
+	var liga []Jogador
+	err := json.NewDecoder(rdr).Decode(&liga)
+	if err != nil {
+		err = fmt.Errorf("Problema parseando a liga, %v", err)
+	}
+	return liga, err
+}
